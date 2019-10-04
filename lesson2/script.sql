f1dba22dc78432c943175f75f2f4a7fd7c39d366
cangreen <latentpickuper@gmail.com>
2019-10-05 01:39:58 +0300

    lesson2

--- lesson2/script.sql
+++ lesson2/script.sql
@@ -1,5 +1,18 @@
 /*
+1. Установите СУБД MySQL. Создайте в домашней директории файл .my.cnf, задав в нем логин и пароль, который указывался при установке.
+2. Создайте базу данных example, разместите в ней таблицу users, состоящую из двух столбцов, числового id и строкового name.
+3. Создайте дамп базы данных example из предыдущего задания, разверните содержимое дампа в новую базу данных sample.
+4. (по желанию) Ознакомьтесь более подробно с документацией утилиты mysqldump. Создайте дамп единственной таблицы
+help_keyword базы данных mysql. Причем добейтесь того, чтобы дамп содержал только первые 100 строк таблицы.
+*/
 
+/*
+~/.my.cnf
+[client]
+host=localhost
+user=root
+password=**********
+socket=/var/run/mysqld/mysqld.sock
 */
 DROP DATABASE if EXISTS example;
 DROP DATABASE if EXISTS sample;
@@ -7,8 +20,9 @@ CREATE DATABASE example;
 CREATE DATABASE sample;
 USE example;
 DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INT(11),
+CREATE TABLE users
+(
+    id   INT(11),
     name VARCHAR(64)
 );
 
